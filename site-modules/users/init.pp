03199eeef1731cead8d59a770b3ca16ecc948571
tjiacobone <82520694+tjiacobone@users.noreply.github.com>
2021-06-08 13:00:02 -0400

    Delete init.pp

--- site-modules/users/init.pp
+++
@@ -1,9 +0,0 @@
-class users {
-  user { 'tj':
-  ensure             => 'present',
-  comment            => 'TJ Iacobone',
-  home               => '/home/tj',
-  password           => 'puppetlabs',
-  shell              => '/bin/bash',
- }
-}
